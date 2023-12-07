42ed940b4ad39e80f97f133ee3a1dd7f5dccdd45
kbdar <70966276+kbdar@users.noreply.github.com>
2023-12-07 19:31:15 +0100

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,8 @@
+URI_STRING = "mongodb+srv://kdar:kdar-pass@clusterkd.thake.mongodb.net/test?retryWrites=true&w=majority"
+DB = "sample_mflix"
+DBTS = "electricity"
+COLLECTION = "embedded_movies"
+COLL_ELEC = "consumption"
+COLL_ELEC_TS = "consumptionTS"
+TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsiMjExMjE3MDc2NDA4NDQiXSwiZXhwIjoxNzk0NTY2ODc2LCJpYXQiOjE3MDAyMTgwNzZ9.w92fArN3khcFdSkXsjaTAfPGWAx3UJjccXSLmd2VnKg'
+ENEDIS_API = 'https://conso.boris.sh/api/consumption_load_curve'
