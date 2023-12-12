e52f2a067c8d6b33c18da87049b3a3160c49f0ba
kbdar <70966276+kbdar@users.noreply.github.com>
2023-12-12 11:43:59 +0100

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,8 +1,6 @@
-URI_STRING = "mongodb+srv://kdar:kdar-pass@clusterkd.thake.mongodb.net/test?retryWrites=true&w=majority"
-DB = "sample_mflix"
-DBTS = "electricity"
-COLLECTION = "embedded_movies"
-COLL_ELEC = "consumption"
-COLL_ELEC_TS = "consumptionTS"
-TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOlsiMjExMjE3MDc2NDA4NDQiXSwiZXhwIjoxNzk0NTY2ODc2LCJpYXQiOjE3MDAyMTgwNzZ9.w92fArN3khcFdSkXsjaTAfPGWAx3UJjccXSLmd2VnKg'
+URI_STRING = "mongodb+srv://<username>:<password>@yourcluster.mongodb.net/test?retryWrites=true&w=majority"
+DBTS = "electricity" #DB name
+COLL_ELEC = "consumption" #normal collection
+COLL_ELEC_TS = "consumptionTS" #timeseries collection
+TOKEN = '<yor enedis token here'
 ENEDIS_API = 'https://conso.boris.sh/api/consumption_load_curve'
